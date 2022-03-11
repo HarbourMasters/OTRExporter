95c95dfde5d5c529c0934d3640149ab635e2e9e5
Nicholas Estelami <NEstelami@users.noreply.github.com>
2022-03-11 18:19:04 -0500

    - Render to target implementation - WIP Texture Cache C++ implementation

--- OTRExporter/ArrayExporter.cpp
+++ OTRExporter/ArrayExporter.cpp
@@ -1,5 +1,6 @@
 #include "ArrayExporter.h"
 #include "VtxExporter.h"
+#include <ZVector.h>
 void OTRExporter_Array::Save(ZResource* res, const fs::path& outPath, BinaryWriter* writer)
 {
 	ZArray* arr = (ZArray*)res;
@@ -24,6 +25,45 @@ void OTRExporter_Array::Save(ZResource* res, const fs::path& outPath, BinaryWrit
 			writer->Write(vtx->g);
 			writer->Write(vtx->b);
 			writer->Write(vtx->a);
+		}
+		else if (arr->resList[i]->GetResourceType() == ZResourceType::Vector)
+		{
+			ZVector* vec = (ZVector*)arr->resList[i];
+			writer->Write((uint32_t)vec->scalarType);
+			writer->Write((uint32_t)vec->dimensions);
+
+			for (int k = 0; k < vec->dimensions; k++)
+			{
+				// OTRTODO: Duplicate code here. Cleanup at a later date...
+				switch (vec->scalarType)
+				{
+				case ZScalarType::ZSCALAR_U8:
+					writer->Write(vec->scalars[k].scalarData.u8);
+					break;
+				case ZScalarType::ZSCALAR_S8:
+					writer->Write(vec->scalars[k].scalarData.s8);
+					break;
+				case ZScalarType::ZSCALAR_U16:
+					writer->Write(vec->scalars[k].scalarData.u16);
+					break;
+				case ZScalarType::ZSCALAR_S16:
+					writer->Write(vec->scalars[k].scalarData.s16);
+					break;
+				case ZScalarType::ZSCALAR_S32:
+					writer->Write(vec->scalars[k].scalarData.s32);
+					break;
+				case ZScalarType::ZSCALAR_U32:
+					writer->Write(vec->scalars[k].scalarData.u32);
+					break;
+				case ZScalarType::ZSCALAR_S64:
+					writer->Write(vec->scalars[k].scalarData.s64);
+					break;
+				case ZScalarType::ZSCALAR_U64:
+					writer->Write(vec->scalars[k].scalarData.u64);
+					break;
+					// OTRTODO: ADD OTHER TYPES
+				}
+			}
 		}
 		else
 		{
