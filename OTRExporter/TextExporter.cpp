ce34ea9cf15cd4cce27a5a556380e019009331ff
Nicholas Estelami <NEstelami@users.noreply.github.com>
2022-03-14 22:52:43 -0400

    Here's your dang Triforce Kenix

--- OTRExporter/TextExporter.cpp
+++ OTRExporter/TextExporter.cpp
@@ -7,8 +7,6 @@ void OTRExporter_Text::Save(ZResource* res, const fs::path& outPath, BinaryWrite
 
 	WriteHeader(txt, outPath, writer, Ship::ResourceType::Text);
 
-	auto start = std::chrono::steady_clock::now();
-
 	writer->Write((uint32_t)txt->messages.size());
 	
 	for (int i = 0; i < txt->messages.size(); i++)
