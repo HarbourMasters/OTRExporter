a299689c6e5e2a2bdeb5702c14ff32bfa1d4980a
louist103 <35883445+louist103@users.noreply.github.com>
2023-11-07 20:36:31 -0500

    Path fix for MM

--- OTRExporter/PathExporter.cpp
+++ OTRExporter/PathExporter.cpp
@@ -1,5 +1,6 @@
 #include "PathExporter.h"
 #include "../ZAPD/ZFile.h"
+#include "Globals.h"
 
 void OTRExporter_Path::Save(ZResource* res, const fs::path& outPath, BinaryWriter* writer)
 {
@@ -12,7 +13,10 @@ void OTRExporter_Path::Save(ZResource* res, const fs::path& outPath, BinaryWrite
 	for (size_t k = 0; k < path->pathways.size(); k++)
 	{
 		writer->Write((uint32_t)path->pathways[k].points.size());
-
+		if (Globals::Instance->game == ZGame::MM_RETAIL) {
+			writer->Write(path->pathways[k].unk1);
+			writer->Write(path->pathways[k].unk2);
+		}
 		for (size_t i = 0; i < path->pathways[k].points.size(); i++)
 		{
 			writer->Write(path->pathways[k].points[i].scalars[0].scalarData.s16);
