b6efce5604a0e4fa0cf4aebd0dfa28bb02f45bbb
inspectredc <inspectredc@gmail.com>
2025-03-28 12:12:11 +0000

    Fix NTSC rom info table

--- rom_info.py
+++ rom_info.py
@@ -41,8 +41,8 @@ ROM_INFO_TABLE[Checksums.OOT_PAL_GC_MQ_DBG] = RomVersion("CFG/filelists/dbg.txt"
 ROM_INFO_TABLE[Checksums.OOT_PAL_10] = RomVersion("CFG/filelists/pal_oot.txt", 0x7950, "N64_PAL_10")
 ROM_INFO_TABLE[Checksums.OOT_PAL_11] = RomVersion("CFG/filelists/pal_oot.txt", 0x7950, "N64_PAL_11")
 ROM_INFO_TABLE[Checksums.OOT_NTSC_10] = RomVersion("CFG/filelists/ntsc_oot.txt", 0x7430, "N64_NTSC_10")
-ROM_INFO_TABLE[Checksums.OOT_NTSC_11] = RomVersion("CFG/filelists/ntsc_oot.txt", 0x7430, "N64_NTSC_10")
-ROM_INFO_TABLE[Checksums.OOT_NTSC_12] = RomVersion("CFG/filelists/ntsc_12_oot.txt", 0x7430, "N64_NTSC_10")
+ROM_INFO_TABLE[Checksums.OOT_NTSC_11] = RomVersion("CFG/filelists/ntsc_oot.txt", 0x7430, "N64_NTSC_11")
+ROM_INFO_TABLE[Checksums.OOT_NTSC_12] = RomVersion("CFG/filelists/ntsc_12_oot.txt", 0x7430, "N64_NTSC_12")
 
 class RomDmaEntry:
     def __init__(self, rom, i):
